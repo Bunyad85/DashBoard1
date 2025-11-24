bce65e982fff868916d4fb61110357151b9e762e
Bunyad85 <bunyad.seferli@mail.ru>
2025-11-24 20:37:43 +0400

    Fix index.jsx file names for Linux/Netlify

--- src/components/GeoGraphyChart.jsx
+++ src/components/GeoGraphyChart.jsx
@@ -4,7 +4,7 @@ import { geoFeatures } from "../data/mockGeoFeatures";
 import { tokens } from "../theme";
 import { mockGeographyData as data } from "../data/mockData";
 
-const GeographyChart = ({ isDashboard = false }) => {
+const GeoGraphyChart = ({ isDashboard = false }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     return (
@@ -82,4 +82,4 @@ const GeographyChart = ({ isDashboard = false }) => {
     );
 };
 
-export default GeographyChart;
+export default GeoGraphyChart;
